fcfe485dbf7f279f8c7a6364ce355d73891b7be6
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-19 12:47:46 +0200

    Task 1

--- src/Homework.java
+++ src/Homework.java
@@ -2,6 +2,17 @@ public class Homework {
 
     public static void main(String[] args) {
 
+        task1();
+
+    }
+
+    private static void task1() {
+
+        System.out.println("Задача 1");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
 
     }
 
